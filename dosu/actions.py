31c90633025b0d11e4b7e4267293cd68de6513c0
Tomáš Sandrini <tomas.sandrini@seznam.cz>
2017-09-24 17:35:27 +0200

    update

---
+++ dosu/actions.py
@@ -0,0 +1,25 @@
+"""
+__/\\\\\\\\\\\\______________________/\\\\\\\\\\\____/\\\________/\\\_
+ _\/\\\////////\\\__________________/\\\/////////\\\_\/\\\_______\/\\\_
+  _\/\\\______\//\\\________________\//\\\______\///__\/\\\_______\/\\\_
+   _\/\\\_______\/\\\_____/\\\\\______\////\\\_________\/\\\_______\/\\\_
+    _\/\\\_______\/\\\___/\\\///\\\_______\////\\\______\/\\\_______\/\\\_
+       _\/\\\\\\\\\\\\/____\///\\\\\/___\///\\\\\\\\\\\/____\///\\\\\\\\\/___
+        _\////////////________\/////_______\///////////________\/////////_____
+
+Created by Tomáš Sandrini
+"""
+
+
+import argparse
+
+
+def ValidateMonths(argparse.action):
+    def __call__(self, parser, args, values, option_string=None):
+        months = [int(month) for month in args]
+        setattr(args, self.dest, months)
+
+def ValidateYears(argparse.action):
+    def __call__(self, parser, args, values, option_string=None):
+        years = [int(year) for years in args]
+        setattr(args, self.dest, years)
